61553e250d1923a38eb13931eb35c5df49f36846
AaryaA31 <100621994+AaryaA31@users.noreply.github.com>
2024-04-30 14:14:20 -0400

    Update brancher.sv

--- brancher.sv
+++ brancher.sv
@@ -1,26 +1,31 @@
-always (@pos_edge clock) begin
-    branch_state <= next_branch_state
-    case (branch_state)
-        2'b00:
-            //Strongly not taken
-            if (branch_taken)
-                next_branch_state = 2'b01
-            if (branch_not_taken)
-                next_branch_state = 2'b00
-        2'b01:
-            if (branch_taken)
-                next_branch_state = 2'b10
-            if (branch_not_taken)
-                next_branch_state = 2'b00
-        2'b10:
-            if (branch_taken)
-                next_branch_state = 2'b11
-            if (branch_not_taken)
-                next_branch_state = 2'b01
-        2'b11:
-            if (branch_taken)
-                next_branch_state = 2'b11
-            if (branch_not_taken)
-                next_branch_state = 2'b10
+module brancher
 
-end
+    always (@pos_edge clock) begin
+        branch_state <= next_branch_state
+        case (branch_state)
+            2'b00:
+                //Strongly not taken
+                if (branch_taken)
+                    next_branch_state = 2'b01
+                if (branch_not_taken)
+                    next_branch_state = 2'b00
+            2'b01:
+                if (branch_taken)
+                    next_branch_state = 2'b10
+                if (branch_not_taken)
+                    next_branch_state = 2'b00
+            2'b10:
+                if (branch_taken)
+                    next_branch_state = 2'b11
+                if (branch_not_taken)
+                    next_branch_state = 2'b01
+            2'b11:
+                if (branch_taken)
+                    next_branch_state = 2'b11
+                if (branch_not_taken)
+                    next_branch_state = 2'b10
+    
+        end
+    end
+endmodule
+                    
