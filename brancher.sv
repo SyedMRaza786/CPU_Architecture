366467a55632c3004d7b66ad983b4da75f4b5751
AaryaA31 <100621994+AaryaA31@users.noreply.github.com>
2024-04-30 14:47:34 -0400

    Update brancher.sv

--- brancher.sv
+++ brancher.sv
@@ -1,31 +1,38 @@
 module brancher
-
+//input reset
+//input clock
+//input take_branch
+//input brancher_valid
+//output branch_prediction
+    
     always (@pos_edge clock) begin
-        branch_state <= next_branch_state
+        if(reset)branch_state=2'b00; 
         case (branch_state)
             2'b00:
                 //Strongly not taken
-                if (branch_taken)
-                    next_branch_state = 2'b01
-                if (branch_not_taken)
-                    next_branch_state = 2'b00
+                if (branch_taken && brancher_valid)
+                    next_branch_state = 2'b01;
+                if (!branch_taken && brancher_valid)
+                    next_branch_state = 2'b00;
             2'b01:
-                if (branch_taken)
-                    next_branch_state = 2'b10
-                if (branch_not_taken)
-                    next_branch_state = 2'b00
+                if (branch_taken && brancher_valid)
+                    next_branch_state = 2'b10;
+                if (!branch_taken && brancher_valid)
+                    next_branch_state = 2'b00;
             2'b10:
-                if (branch_taken)
-                    next_branch_state = 2'b11
-                if (branch_not_taken)
-                    next_branch_state = 2'b01
+                if (branch_taken && brancher_valid)
+                    next_branch_state = 2'b11;
+                if (!branch_taken && brancher_valid)
+                    next_branch_state = 2'b01;
             2'b11:
-                if (branch_taken)
-                    next_branch_state = 2'b11
-                if (branch_not_taken)
-                    next_branch_state = 2'b10
-    
+                if (branch_taken && brancher_valid)
+                    next_branch_state = 2'b11;
+                if (!branch_taken && brancher_valid)
+                    next_branch_state = 2'b10;
         end
+        branch_state <= next_branch_state
+            if(branch_state == 2'b11 || branch_state = 2'b10) branch_prediction = 1;
+            else branch_prediction = 0;
     end
 endmodule
                     
