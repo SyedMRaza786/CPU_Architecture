e9122713f7d4e55d1acacbe50c24577cda8cc6f0
Lauren <38364197+laurenmchin@users.noreply.github.com>
2024-05-01 00:27:19 -0400

    Update regfile.sv

--- cdb_integrated_working/regfile.sv
+++ cdb_integrated_working/regfile.sv
@@ -1,55 +1,62 @@
 /////////////////////////////////////////////////////////////////////////
 //                                                                     //
-//   Modulename :  regfile.sv                                          //
+//  Module Name:  regfile.sv                                           //
 //                                                                     //
-//  Description :  This module creates the Regfile used by the ID and  //
-//                 WB Stages of the Pipeline.                          //
+//  Description:  This module implements a register file for use in    //
+//                the ID & WB stages of a 2-way superscalar pipeline.  //
+//                Notably, this design includes internal forwarding    //
+//                to mitigate RAW hazards without external forwarding  //
+//                logic.                                               //
 //                                                                     //
 /////////////////////////////////////////////////////////////////////////
 
+
 `include "verilog/sys_defs.svh"
 
-// P4 TODO: update this with the new parameters from sys_defs
-
 module regfile (
-    input             clock, // system clock
-    // note: no system reset, register values must be written before they can be read
-    input [4:0]       read_idx_1, read_idx_2, write_idx,
-    input             write_en,
-    input [`XLEN-1:0] write_data,
+  input clock, // system clock
+  // note: no system reset, register values must be written before they can be read
+  input [4:0] readA_idx_1, readB_idx_1,
+  input [4:0] write_idx_1, 
+  input write_en_1, 
+  input [`XLEN-1:0] write_data_1, 
 
-    output logic [`XLEN-1:0] read_out_1, read_out_2
+  output logic [`XLEN-1:0] readA_out_1, readB_out_1, 
 );
 
-    logic [31:1] [`XLEN-1:0] registers; // 31 XLEN-length Registers (0 is known)
+  logic [31:1] [`XLEN-1:0] registers; // 31 XLEN-length Registers (0 is known)
 
-    // Read port 1
-    always_comb begin
-        if (read_idx_1 == `ZERO_REG) begin
-            read_out_1 = 0;
-        end else if (write_en && (write_idx == read_idx_1)) begin
-            read_out_1 = write_data; // internal forwarding
-        end else begin
-            read_out_1 = registers[read_idx_1];
-        end
-    end
+  localparam NUM_REGS = 32;
 
-    // Read port 2
-    always_comb begin
-        if (read_idx_2 == `ZERO_REG) begin
-            read_out_2 = 0;
-        end else if (write_en && (write_idx == read_idx_2)) begin
-            read_out_2 = write_data; // internal forwarding
-        end else begin
-            read_out_2 = registers[read_idx_2];
-        end
-    end
+  // Read ports
+  assign readA_out_1 = (readA_idx_1 == 0) ? 0 :
+                       (write_en_1 && (write_idx_1 == readA_idx_1)) ? write_data_1 :
+                       (write_en_2 && (write_idx_2 == readA_idx_1)) ? write_data_2 :
+                       registers[readA_idx_1];
 
-    // Write port
-    always_ff @(posedge clock) begin
-        if (write_en && write_idx != `ZERO_REG) begin
-            registers[write_idx] <= write_data;
-        end
+  assign readB_out_1 = (readB_idx_1 == 0) ? 0 :
+                       (write_en_1 && (write_idx_1 == readB_idx_1)) ? write_data_1 :
+                       (write_en_2 && (write_idx_2 == readB_idx_1)) ? write_data_2 :
+                       registers[readB_idx_1];
+
+  // Write port
+  always_ff @(posedge clock) begin
+    if (write_en_1 && write_en_2 && write_idx_1 == write_idx_2) begin
+      if (write_idx_1 < NUM_REGS) begin
+        assert(!$isunknown(write_idx_1) && !$isunknown(write_idx_2))
+          else $error("Attempting to write to the same write indices %d %d", write_idx_1, write_idx_2);
+        registers[write_idx_2] <= write_data_2;
+      end else begin
+        $error("Invalid write index: %d", write_idx_1);
+      end
+    end else begin
+      if (write_en_1 && write_idx_1 < NUM_REGS) begin
+        registers[write_idx_1] <= write_data_1;
+      end
+      if (write_en_2 && write_idx_2 < NUM_REGS) begin
+        registers[write_idx_2] <= write_data_2;
+      end
     end
+  end
 
 endmodule // regfile
